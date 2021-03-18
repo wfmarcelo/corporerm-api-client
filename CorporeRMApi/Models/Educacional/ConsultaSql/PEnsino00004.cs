6396281ac1a6b404b96b8aabc3bdbaf85d711f03
marcelo.wollmann <marcelo.wollmann@gmail.com>
2021-03-18 09:38:29 -0300

    inclusao consulta pensino00004

---
+++ CorporeRMApi/Models/Educacional/ConsultaSql/PEnsino00004.cs
@@ -0,0 +1,15 @@
+namespace CorporeRMApi.Models.Educacional.ConsultaSql
+{
+    public class PEnsino00004
+    {
+        public string CursoId { get; set; }
+        public string NomeCurso { get; set; }
+        public string GradeId { get; set; }
+        public string NomeGrade { get; set; }
+        public string TurmaId { get; set; }
+        public string NomeTurma { get; set; }
+        public string TurmaDisciplinaId { get; set; }
+        public string NomeDisciplina { get; set; }
+        public string StatusDisciplina { get; set; }
+    }
+}
